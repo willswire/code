729db423b363bbe9302ca54bb315d830f80736bd
William Walker <w_walker@icloud.com>
2023-08-01 23:27:58 -0500

    solved 1 aug 2023

--- 2023/august/1/main.swift
+++ 2023/august/1/main.swift
@@ -1,5 +1,16 @@
+import Foundation
+
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        
+        var count: Int = 1;
+        for num in nums {
+            for rest in nums.dropFirst(count) {
+                if ((target - num - rest) == 0) {
+                    return [nums.firstIndex(of: num)!, nums.lastIndex(of: rest)!]
+                }
+            }
+            count += 1;
+        }
+        return []
     }
 }
