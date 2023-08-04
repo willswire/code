cc5338f4b26008dddc225e4a8b75747db825390a
William Walker <w_walker@icloud.com>
2023-08-03 21:24:21 -0500

    solved 3 aug 2023

--- 2023/august/3/main.py
+++ 2023/august/3/main.py
@@ -1,23 +1,24 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) != 0:
-            return []
-        
-        keyMap = {
-            "2": ["a", "b", "c"],
-            "3": ["d", "e", "f"],
-            "4": ["g", "h", "i"],
-            "5": ["j", "k", "l"],
-            "6": ["m", "n", "o"],
-            "7": ["p", "q", "r", "s"],
-            "8": ["t", "u", "v"],
-            "9": ["w", "x", "y", "z"]
-        }
-        
-        if len(digits) == 1:
-            return keyMap[digits]
-        else:
-            
+KEYMAP = {
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"]
+}
 
-    
-Solution.letterCombinations(Solution, "23")
+class Solution:
+    def letterCombinations(self, digits: str) -> [str]:
+        if len(digits) == 0:
+            return []
+        elif len(digits) == 1:
+            return KEYMAP[digits]
+        else:
+            rootCombinations = self.letterCombinations(digits[:-1])
+            finalCombinations = []
+            for combination in rootCombinations:
+                for letter in KEYMAP[digits[-1]]:
+                    finalCombinations.append(combination + letter)
+            return finalCombinations
